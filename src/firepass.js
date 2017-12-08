c0b6f9656d764cf06b15b1e0b940ff5f97b9ab9c
Jean Simard <jean.simard@dominos.com>
2017-12-07 19:34:52 -0500

    Update the protocol reading

--- src/firepass.js
+++ src/firepass.js
@@ -1,8 +1,3 @@
-function read(buffer) {
-	let string = JSON.parse(buffer);
-	let message = JSON.parse(string)
-	return message;
-}
 function send(message) {
 	let messageToSend = Buffer.from(JSON.stringify(message), 'utf8');
 	let messageLength = Buffer.alloc(4, 0);
@@ -13,7 +8,7 @@ function send(message) {
 process.stdin.on('data', function(buffer) {
 	let messageReceivedLength = buffer.readInt32LE(0);
 	// TODO: Check that the size of the buffer is the size of the content that has to be read (if incomplete, JSON.parse will fail)
-	let messageReceived = read(buffer.slice(4));
+	let messageReceived = JSON.parse(buffer.slice(4));
 	let message = {
 		count: messageReceived.count,
 		content: 'pong',
