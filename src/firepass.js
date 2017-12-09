6539ced99bfb7ef213fcbc11ada6d0b387b6cdf8
Jean Simard <jean.simard@dominos.com>
2017-12-08 22:01:50 -0500

    Use the new Event Emitter 'Protocol'

--- src/firepass.js
+++ src/firepass.js
@@ -3,26 +3,23 @@ import readline from 'readline';
 
 import protocol from './protocol.js';
 
-process.stdin.on('data', (buffer) => {
-	let messageReceivedLength = buffer.readInt32LE(0);
-	// TODO: Check that the size of the buffer is the size of the content that has to be read (if incomplete, JSON.parse will fail)
-	let messageReceived = JSON.parse(buffer.slice(4));
+protocol.on('message', (input) => {
 	process.env.PATH = `/usr/local/bin:${process.env.PATH}`;
 	let thread = spawn('pass', ['perso/web/test']);
 	let rl = readline.createInterface({
 		"input": thread.stdout,
 		"output": thread.stdin,
 	});
-	let message = {};
+	let output = {};
 	rl.on('line', (line) => {
-		if(!message.password) {
-			message.password = line;
+		if(!output.password) {
+			output.password = line;
 		} else if (line.startsWith('login:')) {
-			message.login = line.replace(/line: /, '');
+			output.login = line.replace(/line: /, '');
 		}
 	});
 	rl.on('pause', () => {
-		protocol.send(message);
+		protocol.send(output);
 		rl.close();
 		thread.kill();
 	});
